2aa0e3f48766774ac1f63ae043784a248c9c1401
JasmineShirely <36262643+JasmineShirely@users.noreply.github.com>
2018-04-28 15:57:45 +0530

    Add files via upload

--- service.ps1
+++ service.ps1
@@ -1,18 +1,19 @@
 $SNowUser = "admin"
 $Systemid="d1bb7314db311300e25dd450cf9619e2" #Paste the Incident's sys ID
-$SNowPass = ConvertTo-SecureString –String "Tinku123(" –AsPlainText -Force
-$SNowCreds = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $SNowUser, $SNowPass
+#$SNowPass = ConvertTo-SecureString –String "Tinku123(" –AsPlainText -Force
+#$SNowCreds = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $SNowUser, $SNowPass
 $headers = @{
 Accept = "application/json"
 }
+$SNowCreds = get-credential
 #connecting to incident
-$jsn = Invoke-RestMethod -Credential $SNowCreds -Headers $headers -Method Get -Uri "https://dev36472.service-now.com/api/now/table/incident/$Systemid"
+$jsn = Invoke-RestMethod -Credential $SNowCreds  -Headers $headers -Method Get -Uri "https://dev36472.service-now.com/api/now/table/incident/$Systemid"
 #extracting values from json
 $id = $jsn.result.short_description
 #$jsn.result.number
 #$id1 = $id -contains 'memory'
 write-host "FETCHING TICKET DETAILS FROM SERVICE NOW"
-write-host "TICKET DESCRIPTION: $id" 
+write-host "TICKET DESCRIPTION: $id"
 $id1= $id -match 'server build'
 "ticket=$id1"| out-file env.properties -encoding ASCII
 #$ticket |out-file env.properties -encoding ASCII
