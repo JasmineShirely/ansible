27f5471d8bb7904f0606a149737c930b168fd8dc
JasmineShirely <36262643+JasmineShirely@users.noreply.github.com>
2018-04-28 16:27:22 +0530

    Add files via upload

--- service.ps1
+++ service.ps1
@@ -1,13 +1,23 @@
-$SNowUser = "admin"
+#$SNowUser = "admin"
 $Systemid="d1bb7314db311300e25dd450cf9619e2" #Paste the Incident's sys ID
 #$SNowPass = ConvertTo-SecureString –String "Tinku123(" –AsPlainText -Force
 #$SNowCreds = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $SNowUser, $SNowPass
-$headers = @{
-Accept = "application/json"
+
+$user = "admin"
+$pass = "Tinku123("
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+$basicAuthValue = "Basic $encodedCreds"
+$Headers = @{
+    Authorization = $basicAuthValue
+    Accept = "application/json"
 }
-$SNowCreds = get-credential
+
+$jsn = Invoke-RestMethod -method Get -Uri "https://dev36472.service-now.com/api/now/table/incident/$Systemid" -Headers $Headers
+#$SNowCreds = get-credential
 #connecting to incident
-$jsn = Invoke-RestMethod -Credential $SNowCreds  -Headers $headers -Method Get -Uri "https://dev36472.service-now.com/api/now/table/incident/$Systemid"
+#$jsn = Invoke-RestMethod -Credential $pair  -Headers $headers -Method Get -Uri "https://dev36472.service-now.com/api/now/table/incident/$Systemid"
 #extracting values from json
 $id = $jsn.result.short_description
 #$jsn.result.number
