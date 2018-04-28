ebfafc908d89103c708e5dda1cbaa9e489a130af
JasmineShirely <36262643+JasmineShirely@users.noreply.github.com>
2018-04-28 15:10:03 +0530

    done
    done

---
+++ service.ps1
@@ -0,0 +1,18 @@
+$SNowUser = "admin"
+$Systemid="d1bb7314db311300e25dd450cf9619e2" #Paste the Incident's sys ID
+$SNowPass = ConvertTo-SecureString –String “Tinku123(” –AsPlainText -Force
+$SNowCreds = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $SNowUser, $SNowPass
+$headers = @{
+Accept = "application/json"
+}
+#connecting to incident
+$jsn = Invoke-RestMethod -Credential $SNowCreds -Headers $headers -Method Get -Uri “https://dev36472.service-now.com/api/now/table/incident/$Systemid”
+#extracting values from json
+$id = $jsn.result.short_description
+#$jsn.result.number
+#$id1 = $id -contains 'memory'
+write-host "FETCHING TICKET DETAILS FROM SERVICE NOW"
+write-host "TICKET DESCRIPTION: $id" 
+$id1= $id -match 'server build'
+"ticket=$id1"| out-file env.properties -encoding ASCII
+#$ticket |out-file env.properties -encoding ASCII
